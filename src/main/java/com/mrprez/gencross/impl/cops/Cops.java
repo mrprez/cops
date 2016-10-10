4e8ac91e394eba081ef28006e5ceed84f071f2b7
Guillaume Gagneret <guillaume.gagneret@woonoz.com>
2016-10-10 15:26:10 +0200

    Continue to work on COPS

--- src/main/java/com/mrprez/gencross/impl/cops/Cops.java
+++ src/main/java/com/mrprez/gencross/impl/cops/Cops.java
@@ -1,9 +1,25 @@
 package com.mrprez.gencross.impl.cops;
 
 import com.mrprez.gencross.Personnage;
+import com.mrprez.gencross.Property;
+import com.mrprez.gencross.value.Value;
 
 public class Cops extends Personnage {
 	
+	public void changeCompetences(Property competence, Value oldValue){
+		if(competence.getSubProperties()!=null){
+			competence.getSubProperties().setFixe(competence.getSubProperties().getDefaultProperty().getValue().getInt() != competence.getValue().getInt());
+		}
+	}
 	
+	public boolean checkCompetences(Property competence, Value newValue){
+		if(competence.getSubProperties()!=null){
+			if(competence.getSubProperties().size()>0){
+				setActionMessage("Vous ne pouvez modifier une compétence avec une ou plusieurs spécialités");
+				return false;
+			}
+		}
+		return true;
+	}
 	
 }

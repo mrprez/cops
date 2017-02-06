7cecdbfc3fa6f10653cf357415cdd1d721b275fe
Guillaume Gagneret <guillaume.gagneret@woonoz.com>
2017-02-06 11:32:53 +0100

    Add migrator for version 1.1

---
+++ src/main/java/com/mrprez/gencross/impl/cops/MigrateFrom1_0.java
@@ -0,0 +1,16 @@
+package com.mrprez.gencross.impl.cops;
+
+import com.mrprez.gencross.Version;
+import com.mrprez.gencross.migration.MigrationPersonnage;
+import com.mrprez.gencross.migration.Migrator;
+
+public class MigrateFrom1_0 implements Migrator {
+
+	@Override
+	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
+		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1, 1));
+		return migrationPersonnage;
+	}
+
+
+}

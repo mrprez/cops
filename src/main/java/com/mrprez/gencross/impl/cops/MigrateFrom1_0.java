ce2535ab8137f12046c5a53f7fad9dcadb2b4c08
Guillaume Gagneret <guillaume.gagneret@woonoz.com>
2017-02-09 15:12:02 +0100

    Add migration for previously solved issue

--- src/main/java/com/mrprez/gencross/impl/cops/MigrateFrom1_0.java
+++ src/main/java/com/mrprez/gencross/impl/cops/MigrateFrom1_0.java
@@ -3,11 +3,21 @@ package com.mrprez.gencross.impl.cops;
 import com.mrprez.gencross.Version;
 import com.mrprez.gencross.migration.MigrationPersonnage;
 import com.mrprez.gencross.migration.Migrator;
+import com.mrprez.gencross.value.IntValue;
 
 public class MigrateFrom1_0 implements Migrator {
 
 	@Override
 	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
+		if (!migrationPersonnage.getPhase().equals("Caractéristiques")
+				&& !migrationPersonnage.getPhase().equals("Compétences de bases")) {
+			migrationPersonnage.getProperty("Compétences#Conduite#Voiture").setEditable(true);
+			if (migrationPersonnage.getPhase().equals("Compétences")) {
+				migrationPersonnage.getProperty("Compétences#Conduite#Voiture").setMax(
+						new IntValue(migrationPersonnage.getProperty("Compétences#Conduite#Voiture").getValue().getInt() - 2));
+			}
+		}
+
 		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1, 1));
 		return migrationPersonnage;
 	}

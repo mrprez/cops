8228ead212f24a8a5e85129f603ec28029b408a2
mrprez <gagneret@yahoo.fr>
2017-02-22 20:31:25 +0100

    New COPS version

---
+++ src/main/java/com/mrprez/gencross/impl/cops/MigrateFrom1_1.java
@@ -0,0 +1,17 @@
+package com.mrprez.gencross.impl.cops;
+
+import com.mrprez.gencross.Version;
+import com.mrprez.gencross.migration.MigrationPersonnage;
+import com.mrprez.gencross.migration.Migrator;
+import com.mrprez.gencross.value.IntValue;
+
+public class MigrateFrom1_1 implements Migrator {
+
+	@Override
+	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
+		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1, 2));
+		return migrationPersonnage;
+	}
+
+
+}
